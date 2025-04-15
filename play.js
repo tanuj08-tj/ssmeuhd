9a4768da6cb22e23708fb4ad54647f136a6bd91c
tanuj08-tj <82132084+tanuj08-tj@users.noreply.github.com>
2025-04-15 10:30:52 +0530

    Update play.js

--- play.js
+++ play.js
@@ -4,7 +4,7 @@ addEventListener('fetch', event => {
 
 async function handleRequest(request) {
   // Real stream URL — keep secret
-  const streamUrl = 'http://alpha-2ott.me:80/play/live.php?mac=A0:BB:3E:26:EA:92&stream=1608071&extension=ts&play_token=aHTaWITAVH'
+  const streamUrl = ''
 
   // Forward the stream to the client
   const response = await fetch(streamUrl, {
